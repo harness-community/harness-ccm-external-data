5dd0ea29dfb46161b30487dc3c1db104764eedc8
Riley Snyder <riley.snyder@harness.io>
2025-06-24 11:17:33 -0500

    Feat/clean data (#1)
    * add ability to render focus data to harness formats
    
    * add custom converters
    
    * add func desc; spelling; field names; provider check
    
    * rename project, move to poetry

---
+++ tests/test_focus_data.py
@@ -0,0 +1,116 @@
+from src.harness_ccm_external_data import Focus, HARNESS_FIELDS
+
+import pandas as pd
+import numpy as np
+
+SAMPLE_DATA = "focus_sample.csv"
+
+# baseline
+test_data = Focus("MyTestPlatform", SAMPLE_DATA)
+test_data_length = 1000
+test_data_col = 44
+
+test_data.render()
+test_data.render_file(f"harness_{SAMPLE_DATA}")
+
+
+def test_data_load():
+    raw_row, raw_col = test_data.billing_content.shape
+    assert raw_row == test_data_length
+    assert raw_col == test_data_col
+
+
+def test_focus_data():
+    focus_row, focus_col = test_data.harness_focus_content.shape
+    assert focus_row == test_data_length
+    assert focus_col == len(HARNESS_FIELDS)
+    assert (
+        test_data.harness_focus_content["EffectiveCost"].sum()
+        == test_data.billing_content["EffectiveCost"].sum()
+    )
+
+
+def test_render_data(tmpdir):
+    filename = tmpdir.join("output.csv")
+
+    test_data.render_file(filename)
+    result_content = pd.read_csv(filename, dtype=str)
+    result_row, result_col = result_content.shape
+
+    assert result_row == test_data_length
+    assert result_col == len(HARNESS_FIELDS)
+
+
+# skipping rows
+def test_data_load_skip_lines():
+    to_skip_lines = 100
+    test_data_skip_lines = Focus("MyTestPlatform", SAMPLE_DATA, skip_rows=to_skip_lines)
+
+    raw_row, raw_col = test_data_skip_lines.billing_content.shape
+    assert raw_row == test_data_length - to_skip_lines
+    assert raw_col == test_data_col
+
+
+def test_data_load_skip_specific_lines():
+    to_skip_specific = [1, 3, 5, 7, 9]
+    test_data_skip_specific = Focus(
+        "MyTestPlatform", SAMPLE_DATA, skip_rows=to_skip_specific
+    )
+
+    raw_row, raw_col = test_data_skip_specific.billing_content.shape
+    assert raw_row == test_data_length - len(to_skip_specific)
+    assert raw_col == test_data_col
+
+
+# cost multiplier
+def test_data_load_multiply():
+    to_multiply = 2
+    test_data_skip = Focus("MyTestPlatform", SAMPLE_DATA, cost_multiplier=to_multiply)
+
+    baseline_sum = pd.read_csv(SAMPLE_DATA)["EffectiveCost"].sum()
+    test_data_skip.render()
+
+    assert (
+        test_data_skip.harness_focus_content["EffectiveCost"].sum()
+        == baseline_sum * to_multiply
+    )
+
+
+# custom converters
+def test_data_convert():
+    test_data_convert = Focus(
+        "MyTestPlatform",
+        SAMPLE_DATA,
+        converters={"BillingAccountId": lambda x: "FakeAccountID"},
+    )
+
+    assert (
+        test_data_convert.billing_content.iloc[0]["BillingAccountId"] == "FakeAccountID"
+    )
+    assert (
+        test_data_convert.billing_content.iloc[test_data_length - 1]["BillingAccountId"]
+        == "FakeAccountID"
+    )
+
+
+# custom converters
+def test_data_null_provider(tmpdir):
+    filename = tmpdir.join("output.csv")
+
+    test_data_setup = Focus(
+        "MyTestPlatform",
+        SAMPLE_DATA,
+        converters={"ProviderName": lambda _: ""},
+    )
+
+    test_data_setup.render_file(filename)
+
+    test_data_provider = Focus(
+        "MyTestPlatformTest",
+        filename,
+    )
+
+    assert (
+        test_data_provider.billing_content.iloc[0]["ProviderName"]
+        == "MyTestPlatformTest"
+    )
