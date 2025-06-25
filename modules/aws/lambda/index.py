bb93bcfc29fff4274d418c0dc5568d01d39d727e
Riley Snyder <riley.snyder@harness.io>
2025-06-25 13:24:52 -0500

    add gitignore, fix files, first working version

--- modules/aws/lambda/index.py
+++ modules/aws/lambda/index.py
@@ -15,34 +15,46 @@ def lambda_handler(event, context):
     s3 = boto3.client('s3')
 
     uploaded_files = []
+    failed_files = []
 
     for record in event["Records"]:
         logging.info(json.dumps(record, indent=2))
 
         provider = getenv("PROVIDER")
-        filename = f'/tmp/{record["s3"]["object"]["key"].split("/")[-1]}'
-        mapping = json.loads(getenv("MAPPING"))
+        filename = record["s3"]["object"]["key"].split("/")[-1]
+        mapping = json.loads(getenv("MAPPING", "{}"))
 
-        s3.download_file(record["s3"]["bucket"]["name"], record["s3"]["object"]["key"], filename)
+        s3.download_file(record["s3"]["bucket"]["name"], record["s3"]["object"]["key"], f"/tmp/{filename}")
 
         focus_data = Focus(
             provider,
-            filename,
-            mapping=mapping if mapping else None
+            f"/tmp/{filename}",
+            mapping=mapping
         )
 
         destination_filename = f"/tmp/processed_{filename}"
 
-        focus_data.render_file(destination_filename)
-
-        # upload file
+        try:
+            focus_data.render_file(destination_filename)
+        except Exception as e:
+            logging.error(record["s3"]["object"]["key"] + ": " + e)
+            failed_files.append(record["s3"]["object"]["key"])
+            continue
+            
+        try:
+            # upload file
+            pass
+        except Exception as e:
+            logging.error(record["s3"]["object"]["key"] + ": " + e)
+            failed_files.append(record["s3"]["object"]["key"])
+            continue
         
-        uploaded_files.append(destination_filename)
+        uploaded_files.append(record["s3"]["object"]["key"])
 
     return {
-        "statusCode": 200,
+        "statusCode": 200 if not failed_files else 400,
         "headers": {
             "Content-Type": "application/json"
         },
-        "body": json.dumps(uploaded_files)
+        "body": json.dumps({"success": uploaded_files, "failure": failed_files})
     }
